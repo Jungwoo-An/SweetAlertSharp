175b4d4894acecea23d47a813b2d82ab743d81d2
Jungwoo An <o1o9814@naver.com>
2019-01-17 00:49:53 +0900

    EnumToVisibleConverter 추가

---
+++ SweetAlertSharp/Converters/EnumToVisibleConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace SweetAlertSharp.Converters
+{
+    public class EnumToVisibleConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+            {
+                return false;
+            }
+
+            var checkValue = value.ToString();
+            var targetValue = parameter.ToString();
+            return checkValue.Equals(targetValue, StringComparison.InvariantCultureIgnoreCase) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+            {
+                return null;
+            }
+
+            var isVisible = (Visibility)value;
+            var targetValue = parameter.ToString();
+            if (isVisible == Visibility.Visible)
+            {
+                return Enum.Parse(targetType, targetValue);
+            }
+
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
